d6ce48d3386c1e144b89f5ef30466902731a521b
Ujjwal Jain <ujjwal9895@gmail.com>
2015-12-09 23:57:09 +0530

    Add code to fetch static json string

--- app/src/main/java/com/example/ujjwaljain/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/ujjwaljain/popularmovies/MainActivityFragment.java
@@ -1,13 +1,24 @@
 package com.example.ujjwaljain.popularmovies;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -32,6 +43,98 @@ public class MainActivityFragment extends Fragment {
                 Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
             }
         });
+
+        FetchMovies fetchMovies = new FetchMovies();
+        fetchMovies.execute();
+
         return rootView;
+    }
+
+    public class FetchMovies extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG = FetchMovies.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader bufferedReader = null;
+            String movieJsonString;
+
+            try {
+
+                String base_URL = "http://api.themoviedb.org/3/discover/movie?";
+
+                Uri uri = Uri.parse(base_URL).buildUpon()
+                        .appendQueryParameter("sort_by","popularity.desc")
+                        .appendQueryParameter("api_key","API_KEY")
+                        .build();
+
+                URL url = new URL(uri.toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer stringBuffer = new StringBuffer();
+
+                if (inputStream == null) {
+                    return null;
+                }
+
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuffer.append(line + "\n");
+                }
+
+                if (stringBuffer.length() == 0) {
+                    return null;
+                }
+
+                movieJsonString = stringBuffer.toString();
+
+                Log.v(LOG_TAG, movieJsonString);
+
+            }catch (IOException e) {
+
+                Log.e(LOG_TAG, "Error closing stream", e);
+                return null;
+
+            }catch (Exception e) {
+
+                Log.e(LOG_TAG, "Error occurred", e);
+                return null;
+
+            }
+            finally {
+
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+
+                if (bufferedReader != null)
+                {
+                    try {
+                        bufferedReader.close();
+                    }catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
     }
 }
